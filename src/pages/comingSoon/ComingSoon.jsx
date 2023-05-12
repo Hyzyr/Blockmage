f01c85b522ca104bbc0aaa67dd13403fe9dfa359
Hyzyr <avezberdiyew@gmail.com>
2023-05-12 12:46:05 +0500

    Blockmage

---
+++ src/pages/comingSoon/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ComingSoon = () => {
+    return (
+        <div className="comingSoon">
+            <div className="frame">
+                
+            </div>
+        </div>
+    );
+};
+
+export default ComingSoon;
